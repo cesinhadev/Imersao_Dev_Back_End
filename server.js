da0b1d5dac629208dd9cad9f2bbc26906c4f17a9
Cesar Augusto <cesinhalius@hotmail.com>
2024-11-18 17:33:50 -0300

    Adicionando comentario e fazendo exercicio proposto

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 import express from "express";
 
 const app = express();
+
+//Iniciando servidor
 app.listen("3000", () => {
     console.log("Estou escutando....");
 });
 
+/* Criando rota para ser comsumida*/
 app.get("/api", (req, res) => {
     res.status(200).send("Boas vinda a imersão Dev!!!");
+});
+
+/* Criando rotas para comsumir dados no formado .json*/
+app.get("/book", (req, res) => {
+    res.status(200).send({
+        "titulo": "Capitão de Areia",
+        "autor": "Jorge Amado",
+        "ano":1937,
+        "Genero": "Ficção"
+    });
 });
