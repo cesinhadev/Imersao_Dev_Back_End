2b32b26011754a1d6b8df8575d699f877b0d817c
Cesar Augusto <cesinhalius@hotmail.com>
2024-11-19 19:48:57 -0300

    Adicionando função de pegar um objeto

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 import express from "express";
 
+const posts = [
+    {
+        id:1,
+        descricao:"foto teste",
+        imagem:"https://placecats.com/millie/300/150"
+    },
+    {
+        id:2,
+        descricao:"gato dormindo",
+        imagem:"https://placecats.com/millie/300/150"
+    },
+    {
+        id:3,
+        descricao:"gato comendo panqueca",
+        imagem:"https://placecats.com/millie/300/150"
+    },
+    {
+        id:4,
+        descricao:"gato fazendo yoga",
+        imagem:"https://placecats.com/millie/300/150"
+    },
+]
+
 const app = express();
+app.use(express.json());
 
 //Iniciando servidor
 app.listen("3000", () => {
@@ -8,16 +32,18 @@ app.listen("3000", () => {
 });
 
 /* Criando rota para ser comsumida*/
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas vinda a imersão Dev!!!");
+app.get("/post", (req, res) => {
+    res.status(200).json(posts);
 });
 
-/* Criando rotas para comsumir dados no formado .json*/
-app.get("/book", (req, res) => {
-    res.status(200).send({
-        "titulo": "Capitão de Areia",
-        "autor": "Jorge Amado",
-        "ano":1937,
-        "Genero": "Ficção"
+// Buscando o objeto pelo id
+function buscarPorID(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
     });
+}
+/* criando rota para devolver unico objeto*/
+app.get("/post/:id", (req,res) =>{
+    const index = buscarPorID(req.params.id);
+    res.status(200).json(posts[index]);
 });
