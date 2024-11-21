ebef0f1bc034f1c54ff72c9db73f587295d60d4d
Cesar Augusto <cesinhalius@hotmail.com>
2024-11-21 15:16:59 -0300

    Adicionando conexão com o MongoDB e configurando

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import conectarAoBanco from "./src/config/dbConfig.js";
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 const posts = [
     {
@@ -31,19 +34,23 @@ app.listen("3000", () => {
     console.log("Estou escutando....");
 });
 
-/* Criando rota para ser comsumida*/
-app.get("/post", (req, res) => {
-    res.status(200).json(posts);
-});
+
+async function getTodosPosts() {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.find().toArray()
+}
+
+
 
 // Buscando o objeto pelo id
-function buscarPorID(id){
+/*function buscarPorID(id){
     return posts.findIndex((post) => {
         return post.id === Number(id);
     });
-}
+}*/
 /* criando rota para devolver unico objeto*/
-app.get("/post/:id", (req,res) =>{
+/*app.get("/post/:id", (req,res) =>{
     const index = buscarPorID(req.params.id);
     res.status(200).json(posts[index]);
-});
+});*/
